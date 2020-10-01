974a6e68b577b1a451eb8c467ecd5e99bb4bb019
Yairish13 <yairish13@gmail.com>
2020-10-01 20:54:48 +0300

    Update OneShow.js

--- movies/src/components/OneShow.js
+++ movies/src/components/OneShow.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 function OneShow() {
+    const [show, setShow] = useState([]);
+
+    useEffect(() => {
+      (async () => {
+        try {
+          const { data } = await axios.get("/https://www.episodate.com/api/show-details?q=29560");
+          setShow(data);
+        } catch (error) {
+          console.log(error.message);
+        }
+      })();
+      console.log(data)
+    }, []);
+
     return (
         <div>
-            
+            <h2>{show.name}</h2>
+            <img src={show.tvShow.image_path} />
+            {show.tvShow.description}
+            seasons:{show.episodes[length-1].season}
         </div>
     )
 } 
